62d82a56ff376eff3dcd04b644a76d88ee2f5c37
cardozog <cardozoguilherme18@gmail.com>
2023-10-25 14:51:57 -0300

    camada de xadrez e imprimindo o tabuleiro

--- sistema_xadrez/src/application/Program.java
+++ sistema_xadrez/src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
-import tabuleiro.Posicao;
+import xadrez.PartidaXadrez;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Posicao pos = new Posicao(3, 5);
-		System.out.println(pos);
+		PartidaXadrez partidaXadrez = new PartidaXadrez();
+		UI.imprimeTabuleiro(partidaXadrez.getPecas());
 
 	}
 
