78764e716bf4773f591db350cc6cc9c7f0f727f5
cardozog <cardozoguilherme18@gmail.com>
2023-10-26 12:11:15 -0300

    Colocando peça no tabuleiro e tratamento com exceções

--- sistema_xadrez/src/tabuleiro/Tabuleiro.java
+++ sistema_xadrez/src/tabuleiro/Tabuleiro.java
@@ -5,6 +5,9 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 
 	public Tabuleiro(int linhas, int colunas) {
+		if (linhas < 1 || colunas < 1) {
+			throw new TabuleiroException("ERRO: Deve ter ao menos uma linha e uma coluna");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -14,24 +17,47 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-
 	public Peca peca(int linha, int coluna) {
+		if (!posicaoExiste(linha, coluna)) {
+			throw new TabuleiroException("Posição inexistente!");
+		}
+
 		return pecas[linha][coluna];
 	}
+
 	public Peca peca(Posicao posicao) {
+
+		if (!posicaoExiste(posicao)) {
+			throw new TabuleiroException("Posição inexistente!");
+		}
+
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 
+	public void colocarPeca(Peca peca,Posicao posicao) {
+		if(temUmaPeca(posicao)) {
+			throw new TabuleiroException("Já tem uma peça nessa posição");
+		}
+		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
+	}
+	
+	
+	public boolean posicaoExiste(int linha, int coluna) {
+		return (linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas);
+
+	}
+
+	public boolean posicaoExiste(Posicao posicao) {
+		return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+
+	}
+
+	public boolean temUmaPeca(Posicao posicao) {
+		return peca(posicao) != null;
+	}
 
 }
